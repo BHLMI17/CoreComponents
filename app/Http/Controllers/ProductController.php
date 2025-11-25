58ecd547215447dff9b97c096f5c622e667589a8
BHLMI17 <bilalhussain.lmi@gmail.com>
2025-11-25 17:09:42 +0000

    index upgrade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,19 +1,27 @@
-// app/Http/Controllers/ProductController.php
+<?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all(); // Fetch products from DB
+        // Fetch all products from the database
+        $products = Product::all();
+
+        // Pass products to the view
         return view('products.index', compact('products'));
     }
 
     public function show($id)
     {
+        // Find product by ID or fail
         $product = Product::findOrFail($id);
+
+        // Pass product to the purchase page
         return view('products.show', compact('product'));
     }
 }
---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image_url',
+        'stock',
+    ];
+
+    /**
+     * Cast attributes to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'price' => 'decimal:2',
+        'stock' => 'integer',
+    ];
+}
