1ac6633183bb238da85647614cfade637ca48c5d
Sami Ahmed <samiahmed.mufc@gmail.com>
2025-12-02 17:19:37 +0000

    Added the LandingPage + Converted the contact page to support PHP

---
+++ public/Landing.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        CoreComponents
+    </title>
+    <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/favicon-96x96.png" type="image/x-icon">
+    <script src="JavaScript/script.js"></script>
+</head>
+
+<link rel = "stylesheet" type="text/css"  href="css/style.css" />
+<head>
+  <body>
+<!-- Header -->
+ <!-- Top Navigation -->
+ <header class="top-bar">
+    <!-- Download the removed bg and cropped png i sent if logo is cooked -->
+  <img src="images/CoreComponentsLogo.png" alt="CoreComponents Logo" class="logo-img" />
+
+  <div class="search-wrapper">
+    <form class="search-bar">
+      <input type="text" placeholder="Search..." />
+      <button type="submit">🔍</button>
+    </form>
+  </div>
+
+  <div class="icon-group">
+    <a href="#" class="icon">🛒</a>
+    <a href="#" class="icon">👤</a>
+  </div>
+</header>
+
+<!-- Lower Navigation -->
+<nav class="nav-bar">
+  <ul class="nav-links">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Browse</a></li>
+    <li><a href="#">Compatibility</a></li>
+    <li><a href="#">About Us</a></li>
+    <li><a href="#">Orders/Returns</a></li>
+  </ul>
+</nav>   
+    </header>
+
+      <main>
+          <section id = "main-thumbnail">
+          <img class = "main-thumbnail" src="images/main-thumbnail.png" alt="Main Thumbnail">
+          </section>
+          
+
+          <section id = "welcome-text">
+            <h1> Welcome to CoreComponents!</h1>
+            <br><br>
+            <h5> Your ultimate destination for high-performance PC parts and upgrades. 
+              We provide everything you need to power your build. Click below to explore our stock 👀👇</h5>
+          </section>
+
+          <section id>
+            <button id ="welcome-button" href="#" class="button">Our Stock</button>
+          </section>
+
+       <footer id="footer">
+          <br>
+          <br>
+      </footer>
+      </main>
+  </body>
+</html>
