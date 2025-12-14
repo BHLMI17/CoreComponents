251e7d140190820bddacf512557647d665e4c346
BHLMI17 <bilalhussain.lmi@gmail.com>
2025-12-14 12:34:21 +0000

    dashboard fix

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/checkout', [BasketController::class, 'checkout'])->name('checkout');
 });
 
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 require __DIR__.'/auth.php';
