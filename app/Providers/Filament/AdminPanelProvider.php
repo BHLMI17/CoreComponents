856b82609e211905764c8e12bd1a889855477671
Sami Ahmed <samiahmed.mufc@gmail.com>
2026-02-15 21:13:24 +0000

    Changed CSS on admin-panel to include our logo + made it match our colour scheme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,13 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin-panel')
             ->path('admin-panel')
             ->login()
+
+            // Branding 
+            ->brandLogo(asset('images/Logo.png')) 
+            ->brandName('CoreComponents')
+
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Green,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
